95330cba750364b39a7a4fe2bc020fc92f43636d
madhanv22 <madhankumarv40@gmail.com>
2024-09-21 13:42:55 -0700

    router base added

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/madhanv22.github.io/madhankumar-protfolio">
       <div className='App'>
         <Header />
         <Routes>
